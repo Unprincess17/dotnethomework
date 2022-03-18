98ebdad34b21b451c65e101db760d95eeb715f02
id_17 <71475755+Unprincess17@users.noreply.github.com>
2022-03-18 13:16:25 +0800

    homework4

---
+++ homework4/Clock/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Clock
+{
+    class Clock
+    {
+        private int hour, minute, second;
+        public int ClockHour  { set; get; }
+        public int ClockMinute { set; get; }
+        public void SetAlarm(int hour, int minute)
+        {
+            if (hour >= 24 || hour < 0 || minute >= 60 || minute < 0)
+                throw new ArgumentOutOfRangeException("invalid input");
+            ClockHour = hour;
+            ClockMinute = minute;
+        }
+        public event Action<int, int, int> OnTick;
+        public event Action<int, int> OnAlarm;
+        public Clock()
+        {
+            hour = DateTime.Now.Hour;
+            minute = DateTime.Now.Minute;
+            second = DateTime.Now.Second;
+        }
+        public void Start()
+        {
+            while (true)
+            {
+                ++second;
+                if (second == 60)
+                {
+                    second = 0;
+                    ++minute;
+                    if (minute == 60)
+                    {
+                        minute = 0;
+                        hour=(hour+1)%24;
+                    }
+                }
+                if (hour == ClockHour && minute == ClockMinute)
+                {
+                    OnAlarm(hour, minute);//in this way it would alarm during HOUR:MIN, so there should jump out of the loop
+                }
+                else OnTick(hour, minute,second);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+    
+    class Program
+    {
+        void OnTick(int hour, int minute, int second)
+        {
+            Console.WriteLine("Tick at {0}:{1}:{2}", hour.ToString("00"), minute.ToString("00"), second.ToString("00"));
+        }
+        void OnAlarm(int hour, int minute)
+        {
+            Console.WriteLine("Alarm at {0}:{1}", hour.ToString("00"), minute.ToString("00"));
+        }
+        static void Main()
+        {
+            Program p = new Program();
+            Clock c1 = new Clock();
+            c1.OnTick += p.OnTick;
+            c1.OnAlarm += p.OnAlarm;
+            c1.SetAlarm(13, 02);
+            c1.Start();
+        }
+    }
+
+    
+}
---
+++ homework4/TemplateList/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TemplateList
+{
+    //public class Node<T>
+    //{
+    //    public Node<T> Next { get; set; }
+    //    public T Data { get; set; }
+    //    public Node(T t)
+    //    {
+    //        Next = null;
+    //        Data = t;
+    //    }
+    //}
+
+    //public class TemplateList<T>
+    //{
+    //    private Node<T> head, tail;
+    //    public TemplateList()
+    //    {
+    //        head = tail = null;
+    //    }
+    //    public Node<T> Head
+    //    {
+    //        get => head;
+    //    }
+
+    //    public void Insert(T t)
+    //    {
+    //        Node<T> node = new Node<T>(t);
+    //        if (tail == null)
+    //        {
+    //            head = tail = node;
+    //        }
+    //        else
+    //        {
+    //            tail.Next = node;
+    //            tail = node;
+    //        }
+    //    }
+
+    //    public delegate int ForEach(Action<T> action);//foreach 和 action之间的关系？ 谁调用foreach？（main）
+        
+    //    public void print(TemplateList<T> L)
+    //    {
+    //        for(Node<T> tmp=L.head; tmp!=null;tmp = tmp.Next)
+    //        {
+    //            Console.WriteLine(tmp.Data);
+    //        }   
+    //    }
+    //}
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<int> l = new List<int>();
+            for(int i = 0; i != 10; ++i)
+            {
+                l.Add(i);
+            }
+            int min=10, max=0, sum=0;
+            l.ForEach(Data=> { 
+                sum += Data;
+                min = Data > min ? min : Data;
+                max = Data < max ? max : Data;
+            });
+        }
+    }
+}
